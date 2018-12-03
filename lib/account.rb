775aad3a0ba8fe1525950a4504a8fdbcf09118a9
felixjtdb <felixjtdb@gmail.com>
2018-12-03 11:11:30 +0000

    deposits are added to an array, when stored they have a timestamp

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,14 @@
 # A class for a bank account, it allows deposits and withdrawls.
 class Account
-  attr_reader :balance
+  attr_reader :balance, :deposits
+
   def initialize
     @balance = 0
+    @deposits = []
   end
 
   def deposit(amount)
     @balance += amount
+    @deposits.push({:amount => amount, :timestamp => Time.now})
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,22 @@
 require "account.rb"
 
 describe Account do
+  let(:a) {described_class.new}
+
   it 'An account has a balance' do
-    a = described_class.new
     expect(a.balance).to eq 0
   end
 
-  it 'A deposit can be made' do
-    a = described_class.new
-    a.deposit(10)
-    expect(a.balance).to eq 10
+  describe "deposit" do
+    it 'A deposit can be made' do
+      a.deposit(10)
+      expect(a.balance).to eq 10
+    end
+
+    it "deposits have a timestamp when created" do
+      allow(Time).to receive(:now).and_return("pass")
+      a.deposit(20)
+      expect(a.deposits.last[:timestamp]).to eq "pass"
+    end
   end
 end
