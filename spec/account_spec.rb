014eb91385b0fc44c1527d1b41cb3d8e338657ee
felixjtdb <felixjtdb@gmail.com>
2018-12-04 14:30:59 +0000

    isolating account tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,43 +2,45 @@ require 'account.rb'
 # testing class Account
 describe Account do
   let(:a) { described_class.new }
+  let(:transaction) { double(balance: 0, credit: 10, debit: 10, timestamp: 'test_time') }
+
+  before do
+    allow(Transaction).to receive(:create).and_return(transaction)
+  end
 
   it 'An account has a balance' do
-    expect(a.balance).to eq 0
+    expect(a.view_balance).to eq '0.00'
   end
 
   describe 'deposit' do
     it 'A deposit increases the balance' do
+      allow(Transaction).to receive(:create).and_return(double(balance: 10))
       a.interact('Deposit', 10)
       expect(a.view_balance).to eq '10.00'
     end
 
     it 'deposits have a timestamp when created' do
-      allow(Time).to receive(:now).and_return('pass')
-      a.interact('Deposit', 20)
-      expect(a.transactions.last.timestamp).to eq 'pass'
+      a.interact('Deposit', 10)
+      expect(a.transactions.last.timestamp).to eq 'test_time'
     end
   end
 
   describe 'withdrawl' do
     it 'withdrawls decrease the balance' do
+      allow(Transaction).to receive(:create).and_return(double(balance: -10))
       expect { a.interact('Withdrawl', 10) }.to change { a.balance }.by(-10)
     end
-    it 'withdrawls decrease the balance' do
-      allow(Time).to receive(:now).and_return('correct')
+
+    it 'withdrawls have a timestamp' do
       a.interact('Withdrawl', 10)
-      expect(a.transactions.last.timestamp).to eq 'correct'
+      expect(a.transactions.last.timestamp).to eq 'test_time'
     end
   end
 
   describe 'statement' do
-    it 'statement prints all transactions' do
-      x = described_class.new
-      allow(Time).to receive(:now).and_return('testing123')
-      x.interact('Deposit', 9)
-      header = "DATE || CREDIT || DEBIT || BALANCE\n"
-      expected_statement = "testing123 || 9.00 || 0.00 || 9.00\n"
-      expect { x.statement }.to output(header + expected_statement).to_stdout
+    it 'statement calls Statement.new' do
+      expect_any_instance_of(Statement).to receive(:print_a)
+      a.statement
     end
   end
 end
